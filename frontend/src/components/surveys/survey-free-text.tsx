ef3bf87397e8cdda5ff82f378bd086f88f5fda07
Osman Melih Tolunay <78886346+Ilovejam@users.noreply.github.com>
2023-07-28 18:39:38 +0300

    Fix freeTextResponses data type

--- frontend/src/components/surveys/survey-free-text.tsx
+++ frontend/src/components/surveys/survey-free-text.tsx
@@ -1,8 +1,12 @@
 import { CheckboxVisibility, DetailsList, Stack } from "@fluentui/react";
 import { FunctionComponent } from "react";
+import surveyResults from "../../data/survey_results"; // JSON verisini import ediyoruz
 
 export const SurveyFreeText: FunctionComponent = () => {
-  const items = ["First item in list", "another one"];
+  const freeTextResponses = (surveyResults.questions
+    .filter((question) => question.type === "text")
+    .map((question) => question.responses) as any[][]) // Dönüştürülmüş tür
+    .reduce((acc, responses) => acc.concat(responses), []);
 
   const _onRenderColumn = (item?: any) => {
     return <div data-is-focusable={true}>{item}</div>;
@@ -11,7 +15,7 @@ export const SurveyFreeText: FunctionComponent = () => {
     <Stack data-testid="FreeTextTable">
       <DetailsList
         checkboxVisibility={CheckboxVisibility.hidden}
-        items={items}
+        items={freeTextResponses}
         columns={[{ key: "Free text", name: "Free text", minWidth: 200 }]}
         ariaLabelForSelectAllCheckbox="Toggle selection for all items"
         ariaLabelForSelectionColumn="Toggle selection"
